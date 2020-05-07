ee203f892681efcc2f1a3faf190cd498029a7130
Vyacheslav <vyacheslavefimov1999@gmail.com>
2020-05-07 20:33:19 +0300

    Initial commit

---
+++ spec/atom-pastebin-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPastebinPluginView from '../lib/atom-pastebin-plugin-view';
+
+describe('AtomPastebinPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
