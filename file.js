50b386074c2ee2b2fa3b29667079d2e85be0e833
Vyacheslav <vyacheslavefimov1999@gmail.com>
2020-05-09 15:55:46 +0300

    Insert paste feature is implemented. Closes #11

--- file.js
+++
@@ -1,7 +0,0 @@
-var PastebinAPI = require('pastebin-js');
-pastebin = new PastebinAPI();
-pastebin.getPaste('GVBPw8Pc').then(function(data) {
-    console.log("good");
-  }).fail(function(err) {
-    console.log("bad");
-  })
